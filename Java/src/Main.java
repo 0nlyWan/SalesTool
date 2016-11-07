3c6f3f46626bfd085f0c75b7d8c8608c1cd2a70b
jk04246 <jk04246@CEIT2552212X031.ad.georgiasouthern.edu>
2016-11-06 20:30:46 -0500

    Add a simple output change.

--- Java/src/Main.java
+++ Java/src/Main.java
@@ -1,3 +1,4 @@
+//Joshua Kim
 public class Main {
 
    public static void main(String[] args) {
@@ -10,6 +11,8 @@ public class Main {
   private static void displayGreeting() {
            System.out.println("HELLO HAPPY SALES PEOPLE!");
            System.out.println("THIS APP SHOWS SALES DATA");
+           
+           System.out.println("Test 1");
   } //End displayGreeting
 
 }  //End class Main
